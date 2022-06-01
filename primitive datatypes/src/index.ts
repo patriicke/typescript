412284d04e867ebf7fd5e4dd2da75e37d5381fce
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-01 15:53:03 +0200

    These are all about the enum data type in typescript

--- primitive datatypes/src/index.ts
+++ primitive datatypes/src/index.ts
@@ -1,12 +1,26 @@
+//numbers
 let sales = 123_345_565;
+//string
 let course = "TypeScript";
+//booleans
 let is_published = true;
-// let level;
-// level = 5;
-// level="a"
+//any
+let level;
+level = 5;
+level = "a";
+//passing arguments as any
 function render(document: any) {
   console.log(document);
 }
+//array
 const arr: number[] = [1, 2, 3, 4];
-
+//tuple
 const person: [number, string] = [1, "Patrick"];
+//enums
+enum Size {
+  Small = 1,
+  Medium,
+  Large
+}
+let mySize: Size = Size.Medium;
+console.log(mySize)

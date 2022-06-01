8a1107f6169d99c277b9910385b92875c59cd84a
patrick-n4 <patrickndayambaje4@gmail.com>
2022-06-01 16:28:26 +0200

    These were the configurations of the typescript made to avoid no unused local, parameter and no implicity return

---
+++ primitive datatypes/dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+let sales = 123345565;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 5;
+level = "a";
+function render(document) {
+    console.log(document);
+}
+const arr = [1, 2, 3, 4];
+const person = [1, "Patrick"];
+let mySize = 2;
+console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2020)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(2000, 4000);
+let employee = {
+    id: 1,
+    name: "",
+    retire: (date) => {
+        console.log(date);
+    }
+};
+//# sourceMappingURL=index.js.map

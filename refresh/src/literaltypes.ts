1bd41af5d7369f1e7eb8bc2377ae09b255dc3b10
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-28 19:45:13 +0200

    I have added the literal types here

---
+++ refresh/src/literaltypes.ts
@@ -0,0 +1,10 @@
+//Literal types in typescript are the
+//Here we want limit values that we can assing to a variable
+//Here are perfect examples that we can use
+
+type Quantity = 50 | 100;
+let quantity: Quantity = 100; //This means that the variable quantity can only be assigned to 50 or 100 only
+
+//Here is a perfect application usnig Metrics
+
+type Metrics = "cm" | "inch"; //This implies that we can only have cm or inch

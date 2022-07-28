e984326f4900b692bcc12accfd2243649125dd2c
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-28 18:25:08 +0200

    I have dealt with number

---
+++ refresh/dist/enum.js
@@ -0,0 +1,10 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+const mySize = Size.Medium;
+console.log(mySize);
+//# sourceMappingURL=enum.js.map
---
+++ refresh/src/enum.ts
@@ -0,0 +1,12 @@
+//Here enums are user defined variables here I'll use the size as an example
+//Remember that we use enum keyword
+//We also use Pascal naming convention
+
+enum Size {
+  Small = 1,
+  Medium,
+  Large
+}
+
+const mySize: Size = Size.Medium;
+console.log(mySize);

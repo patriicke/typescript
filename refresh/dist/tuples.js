48309d506affab11f984ed40101db01a3e01d17f
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-28 18:17:44 +0200

    I have added the tuples

---
+++ refresh/dist/tuples.js
@@ -0,0 +1,3 @@
+"use strict";
+const user = [20, "Patrick"];
+//# sourceMappingURL=tuples.js.map
---
+++ refresh/src/tuples.ts
@@ -0,0 +1,2 @@
+// This is a tuple consisting of number and string where number is representing age and representing name
+const user: [number, string] = [20, "Patrick"];

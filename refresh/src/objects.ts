d7948da259ca9b4b8c5827ed413ac7700d387b8a
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-28 19:07:30 +0200

    I have added the type aliases of type annotation which makes my code easy and good

--- refresh/src/objects.ts
+++ refresh/src/objects.ts
@@ -1,8 +1,10 @@
-const employee: {
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+
+const employee: Employee = {
   id: 1,
   name: "Patrick",
   retire: (date: Date) => {
@@ -10,3 +12,4 @@ const employee: {
   }
 };
 //You can add access modifier to the typescript program like readonly
+// I am going to use type annotation using type keyword, this is and advanced concept since it will follow programming principle DRY: Don't Reapeat Yourself

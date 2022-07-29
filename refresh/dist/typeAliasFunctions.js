d21ba652f418ed9eb1ee0ddb3c50098972676608
patrick-n4 <patrickndayambaje4@gmail.com>
2022-07-29 18:32:36 +0200

    I have tested how to use type alias of functions

--- refresh/dist/typeAliasFunctions.js
+++ refresh/dist/typeAliasFunctions.js
@@ -2,3 +2,6 @@
 const negateFunction = (number) => number * -1;
 const negativeNumber = negateFunction(5);
 console.log(negativeNumber);
+const sumNumbers = (a, b) => a + b;
+let totalSum = sumNumbers(4, 6);
+console.log(totalSum);
--- refresh/src/typeAliasFunctions.ts
+++ refresh/src/typeAliasFunctions.ts
@@ -6,3 +6,12 @@ const negateFunction: Negate = (number) => number * -1;
 
 const negativeNumber = negateFunction(5);
 console.log(negativeNumber);
+
+//Task find the sum of two numbers using type alias
+
+type Sum = (a: number, b: number) => number;
+const sumNumbers: Sum = (a, b) => a + b;
+
+let totalSum = sumNumbers(4, 6);
+
+console.log(totalSum);

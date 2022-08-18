5a14ca090db8ba9b7d804d38782458badb8e52da
patrick-n4 <patrickndayambaje4@gmail.com>
2022-08-18 07:16:33 +0200

    I have added the decorators

---
+++ refresh/src/decorators.ts
@@ -0,0 +1,12 @@
+// function Component(target:Function){
+//     target.prototype.id = 100
+// }
+// @Component
+// export class TestClass {
+//   static elementId: string;
+//   id: number;
+//   printId(prefix: string = ""): string;{
+//     return prefix + this.id;
+//   }
+// }
+// console.log(new TestClass().id)
